5e7d07e15d3ea3eba3799d824167e138dcbde00b
Jacky-Frossard <jacky.frossard@gmail.com>
2026-01-14 09:42:41 +0100

    Update _data/metadata.js (via Pages CMS)

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,5 +1,5 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "Tweaking knobs",
 	url: "https://tweaking-knobs.netlify.app",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
